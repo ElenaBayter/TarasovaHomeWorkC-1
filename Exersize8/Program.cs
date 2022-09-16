bc0c3d83dab8b0e3eae0959ed11eaf7eed7e033a
elena tarasova <nafanichka.ev@gmail.com>
2022-09-16 12:58:10 -0400

    made exersize 8 about numbers from 1 to N

---
+++ Exersize8/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Write a number N: ");
+int n = int.Parse(Console.ReadLine());
+int a = 1;
+while (a <= n)
+{
+    Console.Write($"{a} ");
+    a++;
+}
