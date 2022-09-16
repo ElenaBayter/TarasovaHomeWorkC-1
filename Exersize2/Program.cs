10e7f8bffd6cff0d142651e074c483f2b12d3abd
elena tarasova <nafanichka.ev@gmail.com>
2022-09-16 11:49:36 -0400

    made exersize 2 about max and min numbers

---
+++ Exersize2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("add first number: ");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("add second number: ");
+int number2 = int.Parse(Console.ReadLine());
+
+if (number1>number2)
+{
+    Console.WriteLine("First number is max and second number is min");
+}
+
+else
+{
+    Console.WriteLine("First number is min and second number is max");
+}
